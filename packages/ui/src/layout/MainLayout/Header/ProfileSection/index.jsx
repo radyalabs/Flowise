039e599aa7b66aefef7988fef12075b87e95ed78
Gilang <gilang.taufik@radyalabs.id>
2024-05-20 15:16:36 +0700

    remove about

--- packages/ui/src/layout/MainLayout/Header/ProfileSection/index.jsx
+++ packages/ui/src/layout/MainLayout/Header/ProfileSection/index.jsx
@@ -135,18 +135,7 @@ const ProfileSection = ({ username, handleLogout }) => {
                                                     }
                                                 }}
                                             >
-                                                <ListItemButton
-                                                    sx={{ borderRadius: `${customization.borderRadius}px` }}
-                                                    onClick={() => {
-                                                        setOpen(false)
-                                                        setAboutDialogOpen(true)
-                                                    }}
-                                                >
-                                                    <ListItemIcon>
-                                                        <IconInfoCircle stroke={1.5} size='1.3rem' />
-                                                    </ListItemIcon>
-                                                    <ListItemText primary={<Typography variant='body2'>About Flowise</Typography>} />
-                                                </ListItemButton>
+
                                                 {localStorage.getItem('username') && localStorage.getItem('password') && (
                                                     <ListItemButton
                                                         sx={{ borderRadius: `${customization.borderRadius}px` }}
